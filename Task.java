17f3159d5bad7172757a675ef5a7e1c7de83a9af
shravanishravz94@gmail.com <shravanishravz94@gmail.com>
2018-08-09 18:43:55 -0500

    merging master

--- Task.java
+++ Task.java
@@ -1,5 +1,8 @@
 public class Task {
 	public static void main(String args){
 		System.out.println("Testing Build");
+		System.out.println("Testing Branch");
+		System.out.println("Merging from Master");
+		
 	}
 }
