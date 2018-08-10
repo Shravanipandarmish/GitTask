3abcb0f555617209714193ab625eee76da5e5e52
shravanishravz94@gmail.com <shravanishravz94@gmail.com>
2018-08-10 10:24:17 -0500

    Commit to Mater

--- Task.java
+++ Task.java
@@ -2,7 +2,7 @@ public class Task {
 	public static void main(String args){
 		System.out.println("Testing Build");
 		System.out.println("Testing Branch");
-		System.out.println("Merging from Master");
+		System.out.println("Merging from Master to QA");
 		
 	}
 }
